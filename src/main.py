08bc8b1213761a28de5ecb1f596fc9e5b811b627
R-tanada <r.tanada.550@nitech.jp>
2025-10-25 06:16:56 +0900

    test

--- src/main.py
+++ src/main.py
@@ -4,22 +4,20 @@ from mujoco import viewer
 import time
 
 t0 = time.time()
-model = mujoco.MjModel.from_xml_path("test_mujoco/mujoco_menagerie/franka_fr3/fr3.xml")
+model = mujoco.MjModel.from_xml_path("models/franka_fr3/scene.xml")
 data = mujoco.MjData(model)
 
-data.ctrl[0] = 0.1  # joint1 にコマンド入力
-data.ctrl[1] = -0.1 # joint2 にコマンド入力
-
+cam = mujoco.MjvCamera()
+opt = mujoco.MjvOption()
+scene = mujoco.MjvScene(model, maxgeom=10000)
+context = mujoco.MjrContext(model, mujoco.mjtFontScale.mjFONTSCALE_150)
 
 with viewer.launch_passive(model, data) as v:
+    time.sleep(1)
     while v.is_running():
-        t = time.time() - t0
-        data.ctrl[0] = 0.1*np.sin(t)     # 関節1に周期的な入力：ゲイン0.1
-        data.ctrl[1] = 0.1*np.cos(t)  
+        data.ctrl[0] = 30*np.pi/180
         mujoco.mj_step(model, data)
         v.sync()
-        # angle1 = data.sensordata[0]
-        # velocity1 = data.sensordata[1]
-        # angle2 = data.sensordata[2]
-        # velocity2 = data.sensordata[3]
-        # print(velocity1)
+        test_data = data.sensor('pos1').data
+        print(test_data)
+        print(type(test_data))
